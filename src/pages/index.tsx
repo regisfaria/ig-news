a5da9d03d9d22ddfec609f704517ffb091613148
regisfaria <rgisfaria@yahoo.com.br>
2021-07-26 10:17:01 -0300

    added stripe and fauna connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,6 +50,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,13 +1,12 @@
 import Stripe from 'stripe';
-import { name, version } from '../../package.json';
 
 export const stripe = new Stripe(
-  process.env.STRIPE_KEY,
+  process.env.STRIPE_PRIVATE_KEY,
   {
     apiVersion: '2020-08-27',
     appInfo: {
-      name,
-      version    
+      name: 'ig.news',
+      version: '0.0.1'    
     }
   },
 );
